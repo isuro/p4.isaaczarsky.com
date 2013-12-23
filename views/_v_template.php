02a203e3f22490a7194b54fb6c492fc9b1fe3c1a
Isaac Zarsky <idudez@gmail.com>
2013-12-23 16:43:16 -0500

    basic config

--- views/_v_template.php
+++ views/_v_template.php
@@ -5,6 +5,14 @@
 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
 					
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+	<script src="js/scripts.js"></script>
+
+	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+	<link rel="stylesheet" href="css/styles.css">
+
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
