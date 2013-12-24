3b46a99834f41988ce3c0851f4b775d55f85fa1b
Isaac Zarsky <idudez@gmail.com>
2013-12-24 00:12:17 -0500

    fixes for color changes, began implementing file type detection

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
-$().ready(function(){
-	$.getJSON("http://www.readability.com/api/content/v1/parser?token=349c3efd94e9cebb53cf6697724b6a7dc6797c5c&url=http://www.theverge.com/2013/12/20/5223788/david-x-cohen-end-of-futurama-interview&callback=?", function(data) {
-		$("#content").html(data.content);
+// $().ready(function(){
+// 	$.getJSON("http://www.readability.com/api/content/v1/parser?token=349c3efd94e9cebb53cf6697724b6a7dc6797c5c&url=http://www.theverge.com/2013/12/20/5223788/david-x-cohen-end-of-futurama-interview&callback=?", function(data) {
+// 		$("#content").html(data.content);
+// 		$("#title").html(data.title);
+// 		$("#author").html(data.author);
+// 	});
+// })
+
+function parseUrl() {
+	switch ($("#url-entry").val().slice(-3)){
+
+
+		default:
+			$.getJSON("http://www.readability.com/api/content/v1/parser?token=349c3efd94e9cebb53cf6697724b6a7dc6797c5c&url="+$("#url-entry").val()+"&callback=?", function(data) {
+				$("#content").html(data.content);
+				$("#title").html(data.title);
+				$("#author").html(data.author);
 	});
-})
+	}
+
+
+}
 
 var fontSize = 14;
 var padding = 15;
@@ -58,22 +75,22 @@ function themeChange(){
 	switch ($("#theme-selector").val()){
 		case "day":
 			$("body").css("background-color", "white");
-			$("#content").css("color", "black");
+			$("#story").css("color", "black");
 			$("#controls").removeClass("navbar-inverse");
 			break;
 		case "night":
 			$("body").css("background-color", "black");
-			$("#content").css("color", "white");
+			$("#story").css("color", "white");
 			$("#controls").addClass("navbar-inverse");
 			break;
 		case "solarized-light":
 			$("body").css("background-color", "#fdf6e3");
-			$("#content").css("color", "#586e75");
+			$("#story").css("color", "#586e75");
 			$("#controls").removeClass("navbar-inverse");
 			break;
 		case "solarized-dark":
 			$("body").css("background-color", "#002b36");
-			$("#content").css("color", "#93a1a1");
+			$("#story").css("color", "#93a1a1");
 			$("#controls").addClass("navbar-inverse");
 			break;
 	}
