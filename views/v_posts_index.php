2ff1200d60095a9e9a4f8b9f9958ea11e0dbc4c6
Isaac Zarsky <idudez@gmail.com>
2013-12-28 02:00:31 -0500

    post views for saved links

---
+++ views/v_posts_index.php
@@ -0,0 +1,15 @@
+<?php foreach($posts as $post): ?>
+
+	<article>
+
+		<h1><?=$post['display_name']?> posted:</h1>
+
+		<p><a href="posts/view/<?=$post['post_id']?>"><?=$post['title']?></a></p>
+
+		<time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
+			<?=Time::display($post['created'])?>
+		</time>
+
+	</article>
+
+<?php endforeach; ?>
---
+++ views/v_posts_view.php
@@ -0,0 +1,10 @@
+<div class="container" id="story">
+	<div class="row">
+		<h2 id="title"></h2>
+		<a href="" target="_blank" id="original-link">View original</a>
+		<h4 id="author"></h4>
+	</div>
+	<div class="row">
+		<div id="content" class="col-md-12"></div>
+	</div>
+</div>
