dfa12b45a0cffc292830ae472580e0c0ef40416a
Isaac Zarsky <idudez@gmail.com>
2013-12-28 01:59:26 -0500

    ability to post new links and have basic info fetched

--- controllers/c_base.php
+++ controllers/c_base.php
@@ -27,10 +27,27 @@ class base_controller {
 			$this->template->set_global('user', $this->user);
 
 		# Check if a post was submitted
-			if(isset($_POST['content'])) {
+			if(isset($_POST['source_url'])) {
 
 	      	  	# Associate this post with this user
 				$_POST['user_id']  = $this->user->user_id;
+
+				$headerArray = get_headers($_POST['source_url'], 1);
+				$typeArray = explode("/", $headerArray["Content-Type"]);
+
+				$_POST['media_type'] = $typeArray[0];
+
+				if($_POST['media_type'] == "text") {
+					$parserArray = json_decode(file_get_contents("http://www.readability.com/api/content/v1/parser?token=349c3efd94e9cebb53cf6697724b6a7dc6797c5c&url=".$_POST('url')), true);
+
+					$_POST['title'] = $parserArray['title'];
+					$_POST['content-preview'] = $parserArray['excerpt'];
+
+				}
+
+				else {
+					$_POST['title'] = basename($_POST['source_url']);
+				}
 
 	       	 	# Unix timestamp of when this post was created / modified
 				$_POST['created']  = Time::now();
