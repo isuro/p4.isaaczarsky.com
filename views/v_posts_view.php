03ded1eae2ce29ea8381fac368fdc3196f49eef4
Isaac Zarsky <idudez@gmail.com>
2013-12-28 04:44:11 -0500

    better layout

--- views/v_posts_view.php
+++ views/v_posts_view.php
@@ -1,8 +1,8 @@
 <div class="container" id="story">
 	<div class="row">
 		<h2 id="title"></h2>
-		<a href="" target="_blank" id="original-link">View original</a>
 		<h4 id="author"></h4>
+		<a href="" target="_blank" id="original-link">View original</a>
 	</div>
 	<div class="row">
 		<div id="content" class="col-md-12"></div>
